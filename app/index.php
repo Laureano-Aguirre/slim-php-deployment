1894a545de017e547eb3a85300d3039824646941
Laureano-Aguirre <78192480+Laureano-Aguirre@users.noreply.github.com>
2023-11-11 22:01:45 -0300

    7 commit

--- app/index.php
+++ app/index.php
@@ -5,10 +5,13 @@ ini_set('display_errors', 1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Factory\AppFactory;
 use Slim\Routing\RouteCollectorProxy;
+use Slim\Routing\RouteContext;
 
 require __DIR__ . '/../vendor/autoload.php';
+require_once '../middlewares/Logger.php';
 
 // Instantiate App
 $app = AppFactory::create();
@@ -114,6 +117,6 @@ $app->post('[/]', function (Request $request, Response $response) {
         $response->getBody()->write(json_encode($result));
         return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
     }
-});
+})->add(new LoggerMiddleware());
 
 $app->run();
